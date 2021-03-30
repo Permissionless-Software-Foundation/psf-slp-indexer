d2c8ea4e1e5b8a28496e349cf1bc9e6f5a23ddbd
Chris Troutner <chris.troutner@gmail.com>
2021-03-30 09:33:26 -0700

    Adding rpc directory

---
+++ src/rpc/index.js
@@ -0,0 +1,11 @@
+/*
+  This is the parent class library for the RPC controller.
+*/
+
+class JSONRPC {
+  constructor (localConfig) {
+    console.log('instantiating JSONRPC')
+  }
+}
+
+module.exports = JSONRPC
---
+++ src/rpc/users/index.js
@@ -0,0 +1,11 @@
+/*
+  This is the JSON RPC router for the users API
+*/
+
+class UsersRPC {
+  constructor (localConfig) {
+    console.log('instantiating UsersRPC')
+  }
+}
+
+module.exports = UsersRPC
