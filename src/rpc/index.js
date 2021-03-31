8fb9dc62df28535871d375964404c9c8852d136e
Chris Troutner <chris.troutner@gmail.com>
2021-03-30 19:18:28 -0700

    Got basic parsing in place for JSON RPC

--- src/rpc/index.js
+++ src/rpc/index.js
@@ -2,9 +2,16 @@
   This is the parent class library for the RPC controller.
 */
 
+const jsonrpc = require('jsonrpc-lite')
+
+let _this
+
 class JSONRPC {
   constructor (localConfig) {
-    console.log('instantiating JSONRPC')
+    // Encapsulate dependencies
+    this.jsonrpc = jsonrpc
+
+    _this = this
   }
 
   // This method takes a raw string of data from IPFS, parses it, and determins
@@ -12,9 +19,18 @@ class JSONRPC {
   router (str) {
     try {
       console.log('router str: ', str)
+
+      const parsedData = _this.jsonrpc.parse(str)
+      // console.log('parsedData: ', parsedData)
+
+      // console.log('parsedData.type: ', parsedData.type)
+      // Exit quietly if the incoming string is invalid
+      if (parsedData.type === 'invalid') {
+        return
+      }
     } catch (err) {
-      console.error('Error in rpc router()')
-      throw err
+      console.error('Error in rpc router(): ', err)
+      // Do not throw error. This is a top-level function.
     }
   }
 }
