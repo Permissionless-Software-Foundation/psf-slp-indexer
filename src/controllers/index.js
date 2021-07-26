9dc40a417a9450e365b2c11bcde18da9dca25b06
Chris Troutner <chris.troutner@gmail.com>
2021-07-25 17:30:15 -0700

    Removing comments

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -61,34 +61,4 @@ class Controllers {
   }
 }
 
-// Top-level function for this library.
-// Start the various Controllers and attach them to the app.
-// async function attachControllers (app) {
-//   // Attach the REST controllers to the Koa app.
-//   attachRESTControllers(app)
-//
-//   // Start IPFS.
-//   await adapters.ipfs.start()
-//
-//   attachRPCControllers()
-// }
-//
-// function attachRESTControllers (app) {
-//   const rESTControllers = new RESTControllers({
-//     adapters,
-//     useCases
-//   })
-//
-//   // Attach the REST API Controllers associated with the boilerplate code to the Koa app.
-//   rESTControllers.attachRESTControllers(app)
-// }
-
-// // Add the JSON RPC router to the ipfs-coord adapter.
-// function attachRPCControllers () {
-//   const jsonRpcController = new JSONRPC({ adapters, useCases })
-//
-//   // Attach the input of the JSON RPC router to the output of ipfs-coord.
-//   adapters.ipfs.ipfsCoordAdapter.attachRPCRouter(jsonRpcController.router)
-// }
-
 module.exports = Controllers
