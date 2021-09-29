f181d8b095d21f2fbc2e43aac388686ba1a3a2dd
Chris Troutner <chris.troutner@gmail.com>
2021-09-28 19:52:10 -0700

    fix(docker): Matching ports to dev env

--- production/docker/start-production.sh
+++ production/docker/start-production.sh
@@ -11,10 +11,10 @@ export MNEMONIC="olive two muscle bottom coral ancient wait legend bronze useful
 export COORD_NAME=ipfs-service-provider-generic
 
 # Allow this node to function as a circuit relay. It must not be behind a firewall.
-#export ENABLE_CIRCUIT_RELAY=true
+export ENABLE_CIRCUIT_RELAY=true
 # For browsers to use your circuit realy, you must set up a domain, SSL certificate,
 # and you must forward that subdomain to the IPFS_WS_PORT.
-#export CR_DOMAIN=subdomain.yourdomain.com
+export CR_DOMAIN=subdomain.yourdomain.com
 
 # Debug level. 0 = minimal info. 2 = max info.
 export DEBUG_LEVEL=1
@@ -26,8 +26,9 @@ export DEBUG_LEVEL=1
 export DBURL=mongodb://172.17.0.1:5555/ipfs-service-prod
 
 # Configure IPFS ports
-export IPFS_TCP_PORT=4001
-export IPFS_WS_PORT=4003
+export IPFS_TCP_PORT=5268
+export IPFS_WS_PORT=5269
+
 
 # Configure REST API port
 export PORT=5001
