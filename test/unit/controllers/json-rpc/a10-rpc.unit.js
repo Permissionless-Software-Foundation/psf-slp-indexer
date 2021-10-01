5eb5ef204e9d78a80d16dbd17835b8e05edbb969
Chris Troutner <chris.troutner@gmail.com>
2021-10-01 14:01:08 -0700

    fix(json rpc): Increasing to 100% test coverage

--- test/unit/controllers/json-rpc/a10-rpc.unit.js
+++ test/unit/controllers/json-rpc/a10-rpc.unit.js
@@ -179,5 +179,120 @@ describe('#JSON RPC', () => {
       assert.equal(obj.result.method, 'about')
       assert.equal(obj.id, id)
     })
+
+    it('should exit quietly for duplicate RPC message', async () => {
+      const id = uid()
+      const json = jsonrpc.request(id, 'unknownMethod', {})
+
+      const str = JSON.stringify(json)
+
+      // Call router once.
+      await uut.router(str, 'peerA')
+
+      // Call the router again with the same input.
+      const result = await uut.router(str, 'peerA')
+
+      assert.equal(result, false)
+    })
+
+    it('should ignore metric queries from ipfs-coord', async () => {
+      const id = uid()
+      const json = jsonrpc.request(id, 'unknownMethod', {})
+
+      const str = JSON.stringify(json)
+
+      // Force the RPC type to be 'success', to indicate an RPC that was
+      // processed internal to ipfs-coord.
+      sandbox.stub(uut.jsonrpc, 'parse').returns({
+        payload: {},
+        type: 'success'
+      })
+
+      // Call the router again with the same input.
+      const result = await uut.router(str, 'peerA')
+
+      assert.equal(result, false)
+    })
+
+    it('should report errors when trying to send messages to peers', async () => {
+      const id = uid()
+      const json = jsonrpc.request(id, 'unknownMethod', {})
+
+      const str = JSON.stringify(json)
+
+      // Force issue with sendPrivateMessage()
+      sandbox
+        .stub(uut.ipfsCoord.useCases.peer, 'sendPrivateMessage')
+        .rejects('test error')
+
+      // Call the router again with the same input.
+      const result = await uut.router(str, 'peerA')
+      // console.log('result: ', result)
+
+      assert.property(result, 'from')
+      assert.property(result, 'retStr')
+    })
+  })
+
+  describe('#_checkIfAlreadyProcessed', () => {
+    it('should return false the first time an RPC command is seen', () => {
+      const data = {
+        payload: {
+          jsonrpc: '2.0',
+          id: '6c515f3c-cf8a-42ec-870e-e416edd4923f',
+          method: 'unknownMethod',
+          params: {}
+        },
+        type: 'request'
+      }
+
+      const result = uut._checkIfAlreadyProcessed(data)
+
+      assert.equal(result, false)
+    })
+
+    it('should return true the second time an RPC command is seen', () => {
+      const data = {
+        payload: {
+          jsonrpc: '2.0',
+          id: '6c515f3c-cf8a-42ec-870e-e416edd4923f',
+          method: 'unknownMethod',
+          params: {}
+        },
+        type: 'request'
+      }
+
+      // First call.
+      uut._checkIfAlreadyProcessed(data)
+
+      // Second call.
+      const result = uut._checkIfAlreadyProcessed(data)
+
+      assert.equal(result, true)
+    })
+
+    it('should push out old data from the cache for new data', () => {
+      const data = {
+        payload: {
+          jsonrpc: '2.0',
+          id: '6c515f3c-cf8a-42ec-870e-e416edd4923f',
+          method: 'unknownMethod',
+          params: {}
+        },
+        type: 'request'
+      }
+
+      uut.MSG_CACHE_SIZE = 0
+
+      const result = uut._checkIfAlreadyProcessed(data)
+
+      assert.equal(result, false)
+    })
+
+    it('should return true on error', () => {
+      const result = uut._checkIfAlreadyProcessed()
+
+      assert.equal(result, true)
+    })
   })
 })
