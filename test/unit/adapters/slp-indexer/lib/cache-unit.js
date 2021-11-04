2c2606b0b3ca3dce663f9a26acbb6c0ba5bc13e4
Chris Troutner <chris.troutner@gmail.com>
2021-11-04 15:00:36 -0700

    Fixing unit tests

--- test/unit/adapters/slp-indexer/lib/cache-unit.js
+++ test/unit/adapters/slp-indexer/lib/cache-unit.js
@@ -9,21 +9,28 @@ const BCHJS = require('@psf/bch-js')
 const bchjs = new BCHJS()
 
 const Cache = require('../../../../../src/adapters/slp-indexer/lib/cache')
+const MockLevel = require('../../../../unit/mocks/leveldb-mock')
 
 describe('#cache.js', () => {
   let uut, sandbox
 
   beforeEach(() => {
+    const addrDb = new MockLevel()
+    const tokenDb = new MockLevel()
+    const txDb = new MockLevel()
+
     // Restore the sandbox before each test.
     sandbox = sinon.createSandbox()
 
-    uut = new Cache({ bchjs })
+    uut = new Cache({ bchjs, txDb })
   })
 
   afterEach(() => sandbox.restore())
 
   describe('#get', () => {
     it('should get tx data cache on second call', async () => {
+      sandbox.stub(uut.txDb, 'get').rejects(new Error('not in db'))
+
       // Mock devDependencies
       sandbox
         .stub(uut.transaction, 'get')
