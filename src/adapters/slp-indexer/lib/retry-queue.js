e2fd771eb78f0a2a8c51baedf37a971bbda20c69
Chris Troutner <chris.troutner@gmail.com>
2021-12-16 15:49:37 -0800

    Decreasing retry to 7

--- src/adapters/slp-indexer/lib/retry-queue.js
+++ src/adapters/slp-indexer/lib/retry-queue.js
@@ -23,7 +23,7 @@ class RetryQueue {
     this.pRetry = pRetry
 
     // Note: Retry has exponential back-off, so 6-10 is the right number.
-    this.attempts = 8
+    this.attempts = 7
     this.retryPeriod = 1000
 
     _this = this
