fdaa6cad34d83420742eb09ff0846a06f24f5c46
Chris Troutner <chris.troutner@gmail.com>
2022-01-05 18:08:02 -0800

    Adding RPC info to production startup file

--- production/docker/start-production.sh
+++ production/docker/start-production.sh
@@ -35,6 +35,13 @@ export SVC_ENV=production
 #export IPFS_TCP_PORT=4001
 #export IPFS_WS_PORT=5269
 
+# RPC settings for the full node
+export RPC_IP=172.17.0.1
+export RPC_PORT=8332
+export ZMQ_PORT=28332
+export RPC_USER=bitcoin
+export RPC_PASS=password
+
 # Delete backups as it syncs.
 export DELETE_BACKUP=1
 
