47de8b12d2cac1b59086d1544a54c8f8e2a73215
Chris Troutner <chris.troutner@gmail.com>
2022-01-05 17:46:59 -0800

    fix(production): Working on building docker container

---
+++ production/docker/restore-auto.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf /home/safeuser/psf-slp-indexer/leveldb/current/*
+
+cp -r /home/safeuser/psf-slp-indexer/leveldb/zips/home/safeuser/psf-slp-indexer/src/adapters/slp-indexer/lib/leveldb/current/* /home/safeuser/code/psf-slp-indexer/leveldb/current/
