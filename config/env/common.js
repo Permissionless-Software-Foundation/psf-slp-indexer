3136a0b435ee7be8f7dfa6686580a2aaccdb1e79
Chris Troutner <chris.troutner@gmail.com>
2022-01-10 14:05:07 -0800

    fix(blacklist): Allowing override with env var DISABLE_BLACKLIST

--- config/env/common.js
+++ config/env/common.js
@@ -97,8 +97,10 @@ module.exports = {
     : 5001,
 
   // This blacklist is used to ignore problematic tokens.
-  blacklist: [
-    // FlexUSD
-    'dd21be4532d93661e8ffe16db6535af0fb8ee1344d1fef81a193e2b4cfa9fbc9'
-  ]
+  blacklist: process.env.DISABLE_BLACKLIST
+    ? [
+        // FlexUSD
+        'dd21be4532d93661e8ffe16db6535af0fb8ee1344d1fef81a193e2b4cfa9fbc9'
+      ]
+    : []
 }
