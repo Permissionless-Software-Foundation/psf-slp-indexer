5133a99d8fcd0ae4885722360034cf7fc23a9654
Chris Troutner <chris.troutner@gmail.com>
2022-04-24 14:47:16 -0700

    fix(backups): Preserving 5 backups by default

--- config/env/common.js
+++ config/env/common.js
@@ -102,5 +102,8 @@ module.exports = {
     : [
         // FlexUSD
         'dd21be4532d93661e8ffe16db6535af0fb8ee1344d1fef81a193e2b4cfa9fbc9'
-      ]
+      ],
+
+  // Number of backups to retain.
+  backupQty: process.env.BACKUP_QTY ? parseInt(process.env.BACKUP_QTY) : 5
 }
